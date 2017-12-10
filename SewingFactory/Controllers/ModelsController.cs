5efc3b60ca6af532a32cae29cdb4d4244ae727f2
Alexey <gotmanalexey@gmail.com>
2017-12-10 23:37:30 +0600

    update model

--- SewingFactory/Controllers/ModelsController.cs
+++ SewingFactory/Controllers/ModelsController.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using SewingFactory.Models;
 
 namespace SewingFactory.Controllers
 {
-    public class ModelsController : Controller
+    public class ModelController : Controller
     {
         private SewingFactoryEntities db = new SewingFactoryEntities();
 
-        // GET: Models
+        // GET: /Model/
         public ActionResult Index()
         {
             var model = db.Model.Include(m => m.CatModel);
             return View(model.ToList());
         }
 
-        // GET: Models/Details/5
+        // GET: /Model/Details/5
         public ActionResult Details(short? id)
         {
             if (id == null)
@@ -36,16 +32,16 @@ namespace SewingFactory.Controllers
             return View(model);
         }
 
-        // GET: Models/Create
+        // GET: /Model/Create
         public ActionResult Create()
         {
             ViewBag.Category = new SelectList(db.CatModel, "Id", "Name");
             return View();
         }
 
-        // POST: Models/Create
+        // POST: /Model/Create
         // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Category,Name")] Model model)
@@ -61,7 +57,7 @@ namespace SewingFactory.Controllers
             return View(model);
         }
 
-        // GET: Models/Edit/5
+        // GET: /Model/Edit/5
         public ActionResult Edit(short? id)
         {
             if (id == null)
@@ -77,9 +73,9 @@ namespace SewingFactory.Controllers
             return View(model);
         }
 
-        // POST: Models/Edit/5
+        // POST: /Model/Edit/5
         // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
-        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Category,Name")] Model model)
@@ -94,7 +90,7 @@ namespace SewingFactory.Controllers
             return View(model);
         }
 
-        // GET: Models/Delete/5
+        // GET: /Model/Delete/5
         public ActionResult Delete(short? id)
         {
             if (id == null)
@@ -109,7 +105,7 @@ namespace SewingFactory.Controllers
             return View(model);
         }
 
-        // POST: Models/Delete/5
+        // POST: /Model/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(short id)
